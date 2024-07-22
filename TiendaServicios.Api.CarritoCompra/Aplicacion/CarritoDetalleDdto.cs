16b6cdfdc9a6777422e1755fba8fcddddc93c0bc
Willuww <joelbelloromero@gmail.com>
2024-07-22 15:54:07 -0600

    Agregar archivos de proyecto.

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/CarritoDetalleDdto.cs
@@ -0,0 +1,10 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class CarritoDetalleDdto
+    {
+        public Guid? LibroId { get; set; }
+        public string TituloLibro { get; set; }
+        public string AutorLibro { get; set; }
+        public DateTime? FechaPublicacion { get; set; }
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/Modelo/CarritoSesion.cs
@@ -0,0 +1,9 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesion
+    {
+        public int CarritoSesionId { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public ICollection<CarritoSesionDetalle> ListaDetalle { get; set; }
+    }
+}
